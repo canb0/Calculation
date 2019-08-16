d7ca605c58607472d58c31dbceb092fcabaeda54
Canberk <Gulin-2013>
2019-08-16 20:39:16 +0200

    added surefire

--- src/test/java/com/swtest/calc/TestFactory.java
+++ src/test/java/com/swtest/calc/TestFactory.java
@@ -1,35 +1,39 @@
 package com.swtest.calc;
 
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(DataProviderRunner.class)
 public class TestFactory {
 
-	BetCalculator scoreCalculator;
+	private static BetCalculator scoreCalculator;
 
-	@DataProvider(name = "guessScore")
-	public static Object[][] data_guess_result() {
+	@DataProvider
+	public static Object[][] guessScore() {
 		return new Object[][] { { "2-1", 1 }, { "2-2", 0 }, { "1-1", 0 },
 				{ "1-2", 2 }, { "0-0", 0 } };
 	}
 
-	@DataProvider(name = "totalGoal")
-	public static Object[][] data_total_goal() {
+	@DataProvider
+	public static Object[][] totalGoal() {
 		return new Object[][] { { "2-1", 3 }, { "2-2", 4 }, { "1-1", 2 },
 				{ "5-8", 13 }, { "1-0", 1 }, { "0-0", 0 }, { "", -1 } };
 	}
 
-	@DataProvider(name = "equalScore")
-	public static Object[][] data_equal_score() {
+	@DataProvider
+	public static Object[][] equalScore() {
 		return new Object[][] { { "2-1", "2-1", true }, { "1-2", "1-2", true },
 				{ "0-0", "0-0", true }, { "2-2", "2-1", false },
 				{ "1-1", "2-1", false }, { "2-1", "0-0", false },
 				{ "3-1", "1-3", false } };
 	}
 
-	@DataProvider(name = "totalpoint")
+	@DataProvider
 	public static Object[][] totalPoint() {
 		return new Object[][] { { "2-1", "2-1", 6 }, { "2-1", "1-1", 0 },
 				{ "1-2", "1-3", 3 }, { "2-0", "2-0", 5 }, { "0-0", "0-0", 4 },
@@ -37,26 +41,29 @@ public class TestFactory {
 	}
 
 	@BeforeClass
-	public void Before() {
+	public static void Before() {
 
 		scoreCalculator = BetCalculator.getInstance();
 	}
 
-	@Test(dataProvider = "guessScore")
+	@Test
+	@UseDataProvider("guessScore")
 	public void testWinner(String score, int result) {
 		Match m = new Match(score);
 		scoreCalculator.setMatchs(m, null);
 		Assert.assertEquals(result, scoreCalculator.findWinner(m));
 	}
 
-	@Test(dataProvider = "totalGoal")
+	@Test
+	@UseDataProvider("totalGoal")
 	public void testTotalGoal(String score, int result) {
 		Match m = new Match(score);
 		scoreCalculator.setMatchs(m, null);
 		Assert.assertEquals(result, scoreCalculator.findGoalNumber(m));
 	}
 
-	@Test(dataProvider = "equalScore")
+	@Test
+	@UseDataProvider("equalScore")
 	public void testScore(String realScore, String userScore, boolean result) {
 		Match reamM = new Match(realScore);
 		Match userM = new Match(userScore);
@@ -64,7 +71,8 @@ public class TestFactory {
 		Assert.assertEquals(result, scoreCalculator.matchScore(reamM, userM));
 	}
 
-	@Test(dataProvider = "totalPoint")
+	@Test
+	@UseDataProvider("totalPoint")
 	public void totalPoint(String realScore, String userScore, int totalPoint) {
 		Match reamM = new Match(realScore);
 		Match userM = new Match(userScore);

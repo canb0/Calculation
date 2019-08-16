75713537646b801358c07342bb18ea2e9e6509bd
Canberk <Gulin-2013>
2019-08-16 19:42:46 +0200

    removed junit added testng

--- src/test/java/com/swtest/calc/TestFactory.java
+++ src/test/java/com/swtest/calc/TestFactory.java
@@ -1,6 +1,6 @@
 package com.swtest.calc;
 
-import junit.framework.Assert;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
